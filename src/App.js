622a1483eaf7bbb36a4afc90d674984f31e82d51
Arc1el <khm970514@gmail.com>
2020-11-24 00:53:32 +0900

    making new components and using props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
-function App() {
+
+function Food({fav})
+{
+  return(
+    <h1>i like {fav}</h1>
+  );
+}
+function App()
+{
   return (
     <div>
       <h1>Hello</h1>
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="chukumi"/>
+      <Food fav="samgiopsal"/>
     </div>
   );
 }
